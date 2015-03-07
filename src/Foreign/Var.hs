4f2f0ce5bf77353604915c80d8dbed59aef5673b
Edward Kmett <ekmett@gmail.com>
2015-03-07 17:12:48 -0500

    Tweak HasUpdate so users who prefer a profunctor settable could use it. Missing instances, code shuffling, haddocks, added updates to Var

--- src/Foreign/Var.hs
+++ src/Foreign/Var.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
 --------------------------------------------------------------------
 -- |
 -- Copyright :  (c) 2014-2015 Edward Kmett
@@ -13,14 +14,17 @@
 --
 --------------------------------------------------------------------
 module Foreign.Var
-  ( HasGetter(get)
-  , GettableVar
-  , HasSetter(($=))
-  , SettableVar(SettableVar)
-  , ($=!)
-  , Var(Var)
+  (
+  -- * Variables
+    Var(Var)
+  , newVar
   , mapVar
+  , SettableVar(SettableVar)
+  , GettableVar
+  -- * Classes
+  , HasSetter(($=)), ($=!)
   , HasUpdate(($~), ($~!))
+  , HasGetter(get)
   ) where
 
 import Control.Concurrent.STM
@@ -34,27 +38,34 @@ import Foreign.Storable
 --import Data.Functor.Contravariant
 --import Data.Functor.Contravariant.Divisible
 
-infixr 2 $=, $=!, $~, $~!
+--------------------------------------------------------------------
+-- * Var
+--------------------------------------------------------------------
 
-class HasSetter t a | t -> a where
-  ($=) :: MonadIO m => t -> a -> m ()
+data Var a = Var
+  (IO a)       -- get
+  ((a -> a) -> IO ()) -- update
+  ((a -> a) -> IO ()) -- strict update
+  (a -> IO ()) -- set
+  deriving Typeable
 
-instance Storable a => HasSetter (Ptr a) a where
-  p $= a = liftIO $ poke p a
-  {-# INLINE ($=) #-}
+-- | Build a 'Var' form a getter and a setter.
+newVar :: (IO a) -> (a -> IO ()) -> Var a
+newVar g s = Var g u u' s where
+  u f = do
+    a <- g
+    s (f a)
+  u' f = do
+    a <- g
+    s $! f a
 
-instance HasSetter (IORef a) a where
-  p $= a = liftIO $ writeIORef p a
-  {-# INLINE ($=) #-}
+-- | Change the type of a 'Var'
+mapVar :: (b -> a) -> (a -> b) -> Var a -> Var b
+mapVar ba ab (Var ga ua ua' sa) = Var (ab <$> ga) (\bb -> ua (ba . bb . ab)) (\bb -> ua' (ba . bb . ab)) (sa . ba)
+{-# INLINE mapVar #-}
 
-instance HasSetter (TVar a) a where
-  p $= a = liftIO $ atomically $ writeTVar p a
-
-($=!) :: (HasSetter t a, MonadIO m) => t -> a -> m ()
-p $=! a = (p $=) $! a
-{-# INLINE ($=!) #-}
-
-newtype SettableVar a = SettableVar (a -> IO ()) deriving Typeable
+newtype SettableVar a = SettableVar (a -> IO ())
+  deriving Typeable
 
 {-
 instance Contravariant SettableVar where
@@ -73,14 +84,84 @@ instance Decidable SettableVar where
     Right c -> r c
 -}
 
+type GettableVar = IO
+
+--------------------------------------------------------------------
+-- * HasSetter
+--------------------------------------------------------------------
+
+infixr 2 $=, $=!
+
+class HasSetter t a | t -> a where
+  ($=) :: MonadIO m => t -> a -> m ()
+
+($=!) :: (HasSetter t a, MonadIO m) => t -> a -> m ()
+p $=! a = (p $=) $! a
+{-# INLINE ($=!) #-}
+
 instance HasSetter (SettableVar a) a where
   SettableVar f $= a = liftIO (f a)
   {-# INLINE ($=) #-}
 
-type GettableVar = IO
+instance HasSetter (Var a) a where
+  Var _ _ _ s $= a = liftIO $ s a
+
+instance Storable a => HasSetter (Ptr a) a where
+  p $= a = liftIO $ poke p a
+  {-# INLINE ($=) #-}
+
+instance HasSetter (IORef a) a where
+  p $= a = liftIO $ writeIORef p a
+  {-# INLINE ($=) #-}
+
+instance HasSetter (TVar a) a where
+  p $= a = liftIO $ atomically $ writeTVar p a
+
+--------------------------------------------------------------------
+-- * HasUpdate
+--------------------------------------------------------------------
+
+infixr 2 $~, $~!
+
+class HasSetter t a => HasUpdate t a b | t -> a b where
+  ($~) :: MonadIO m => t -> (a -> b) -> m ()
+  default ($~) :: (MonadIO m, a ~ b, HasGetter t a, HasSetter t a) => t -> (a -> b) -> m ()
+  r $~ f = liftIO $ do
+    a <- get r
+    r $= f a
+  ($~!) :: MonadIO m => t -> (a -> b) -> m ()
+  default ($~!) :: (MonadIO m, a ~ b, HasGetter t a, HasSetter t a) => t -> (a -> b) -> m ()
+  r $~! f = liftIO $ do
+    a <- get r
+    r $=! f a
+
+instance HasUpdate (Var a) a a where
+  Var _ u _ _ $~  f = liftIO $ u f
+  Var _ _ v _ $~! f = liftIO $ v f
+
+instance Storable a => HasUpdate (Ptr a) a a
+
+instance HasUpdate (IORef a) a a where
+  r $~ f  = liftIO $ atomicModifyIORef r $ \a -> (f a,())
+  r $~! f = liftIO $ atomicModifyIORef' r $ \a -> (f a,())
+
+instance HasUpdate (TVar a) a a where
+  r $~ f = liftIO $ atomically $ do
+    a <- readTVar r
+    writeTVar r (f a)
+  r $~! f = liftIO $ atomically $ do
+    a <- readTVar r
+    writeTVar r $! f a
+
+--------------------------------------------------------------------
+-- * HasGetter
+--------------------------------------------------------------------
 
 class HasGetter t a | t -> a where
   get :: MonadIO m => t -> m a
+
+instance HasGetter (Var a) a where
+  get (Var g _ _ _) = liftIO g
 
 instance HasGetter (TVar a) a where
   get = liftIO . atomically . readTVar
@@ -96,43 +177,3 @@ instance Storable a => HasGetter (Ptr a) a where
 
 instance HasGetter (IORef a) a where
   get = liftIO . readIORef
-
-data Var a = Var (IO a) (a -> IO ()) deriving Typeable
-
-instance HasGetter (Var a) a where
-  get (Var g _) = liftIO g
-
-instance HasSetter (Var a) a where
-  Var _ s $= a = liftIO $ s a
-
-mapVar :: (b -> a) -> (a -> b) -> Var a -> Var b
-mapVar ba ab (Var ga sa) = Var (ab <$> ga) (sa . ba)
-{-# INLINE mapVar #-}
-
-class HasSetter t a => HasUpdate t a | t -> a where
-  ($~) :: MonadIO m => t -> (a -> a) -> m ()
-  default ($~) :: (MonadIO m, HasGetter t a, HasSetter t a) => t -> (a -> a) -> m ()
-  r $~ f = liftIO $ do
-    a <- get r
-    r $= f a
-  ($~!) :: MonadIO m => t -> (a -> a) -> m ()
-  default ($~!) :: (MonadIO m, HasGetter t a, HasSetter t a) => t -> (a -> a) -> m ()
-  r $~! f = liftIO $ do
-    a <- get r
-    r $=! f a
-
-instance HasUpdate (Var a) a
-
-instance Storable a => HasUpdate (Ptr a) a
-
-instance HasUpdate (IORef a) a where
-  r $~ f  = liftIO $ atomicModifyIORef r $ \a -> (f a,())
-  r $~! f = liftIO $ atomicModifyIORef' r $ \a -> (f a,())
-
-instance HasUpdate (TVar a) a where
-  r $~ f = liftIO $ atomically $ do
-    a <- readTVar r
-    writeTVar r (f a)
-  r $~! f = liftIO $ atomically $ do
-    a <- readTVar r
-    writeTVar r $! f a
